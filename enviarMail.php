9083d2d7d8846bd18e536ebb2101e9bb1148b925
Luciano Gnocchi <emilu11@hotmail.com>
2016-04-01 22:05:18 -0300

    EnviarMail ready

--- enviarMail.php
+++ enviarMail.php
@@ -1,7 +1,17 @@
 <?php
- 
- if (isset($_GET['mail'])){
- 		
+
+ $f="archivos/Lista_de_Precios_El_Oriente.pdf"   ;
+     header("Content-type: application/octet-stream");
+    header("Content-Disposition: attachment; filename=\"$f\"\n");
+    $fp=fopen("$f", "r");
+    fpassthru($fp);
+    
+    if(isset($_POST["correo"]) ){
+        $correo=$_POST["correo"];
+       $telefono=$_POST["telefono"]; 
+    }
+		
+        $nombre="Admin";
         $fecha=date("d-m-Y");
         $hora=date("H:i:s");      
         $asunto='Compra en x dominio';
@@ -10,21 +20,19 @@
         $correo="grijalvaromero@gmail.com";
         /*Debe de indicar los estilos css aqui mismo en la variable  y si quieres incluir imagenes,estas tendran que 
         estar en un servidor yo tome la de google */
-       $comentario='Se correo : '.$_GET['mail'].' y con telefono: '.$get['Telefono'].' a descargado el formulario de precios a las '.$hora.' del dia '.$fecha;
+       $comentario='Hola '.$nombre.' el usuario con mail:'.$correo.' y telefono '.$telefono.' a descargado el listado de precios desde elOriente '; 
+
+
+        //para el envío en formato HTML 
+         echo $comentario;
         $headers = "MIME-Version: 1.0\r\n"; 
-        $headers .= "Content-type: text/html; charset=utf8\r\n"; 
+            $headers .= "Content-type: text/html; charset=utf8\r\n"; 
 
         //dirección del remitente 
-        $headers .= "From: El Oriente\r\n"; 
-        
+        $headers .= "From: Remitente\r\n"; 
         //FUNCION PARA ENVIAR EL EMAIL
         mail('lucianognocchi@gmail.com',$asunto,$comentario,$headers);
-        $f = 'Lista_de_Precios_El_Oriente.pdf';
-    header("Content-type: application/octet-stream");
-    header("Content-Disposition: attachment; filename=\"$f\"\n");
-    $fp=fopen("$f", "r");
-    fpassthru($fp);
-
-       }
-      // header("Location:".$_SERVER['HTTP_REFERER']);
 ?>
+
+
+
